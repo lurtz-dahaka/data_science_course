cd05bfd539cc29535f6db10578c44df9e64449f2
Egor Krasnikov <lurtz.dahaka@gmail.com>
2023-08-16 21:19:27 +0300

    Prod DS 1 update

---
+++ SKILLFACTORY/DS_PROD-1. Подготовка модели к продакшену и деплой/server.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route('/hello')
+def hello_func():
+    return 'hello!'
+
+if __name__ == '__main__':
+
+    app.run('localhost', 5000)
+from server import *
